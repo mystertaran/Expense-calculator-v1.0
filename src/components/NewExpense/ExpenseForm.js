a124f654f9f74a32cf13b581d9c57d7ef4659fd8
mystertaran <sz.taranczewski@gmail.com>
2023-09-21 15:09:41 +0200

    adding listeners

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,27 +1,45 @@
+import { useState } from "react";
+
 import "./ExpenseForm.css";
 
 function ExpenseForm() {
+  
+  const [enteredTitle, setEnteredTitle] = useState('')
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const [enteredAmount, setEnteredAmount] = useState('')
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const [enteredDate, setEnteredDate] = useState('')
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
         </div>
         <div className="new-expense__control">
           <label>date</label>
-          <input type="date" min="2018-01-01" max="2050-01-01" />
+          <input type="date" min="2018-01-01" max="2050-01-01" onChange={dateChangeHandler}/>
         </div>
       </div>
       <div className="new-expense__actions">
         <button type="submit">Add Expense</button>
       </div>
     </form>
-  );
+  )
 }
 
 export default ExpenseForm;
