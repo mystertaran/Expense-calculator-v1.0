e285cbc15f6cc066e9593f3733aa578592b40b08
mystertaran <sz.taranczewski@gmail.com>
2023-09-25 23:20:25 +0200

    adding filter options (in progress)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,21 @@
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css"
+import { useState } from "react";
 
 
-function Expenses(props) {
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('');
+  
+  const addFilterByYear = (newYear) => {
+    setFilteredYear(newYear);
+    console.log(`Here is from Expenses ${newYear}`)
+  }
+
  return (
     <Card className="expenses">
+        <ExpensesFilter showYearHandler={addFilterByYear} />
         <ExpenseItem
         title={props.data[0].title}
         amount={props.data[0].amount}
